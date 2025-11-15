a7719714c983fca375af6a13b39928a852701345
Ni_09 <NinthIsland09@gmail.com>
2025-11-15 18:09:56 -0500

    Update API key info

--- src/APIKey.java
+++ src/APIKey.java
@@ -1,6 +1,10 @@
 public class APIKey {
 
 
-    // THIS API KEY HAS BEEN REVOKED. NOT A REAL API KEY!!
+    // insert API key here
+
     public static String API_KEY = "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";
+
+    // older commits have an API key here. It has been revoked and is no longer a concern. 
+
 }
